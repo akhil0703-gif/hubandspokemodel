fa5aa4fa911b912d3a7ea9fb9f1ea791cd5577e7
akhil0703-gif <akhilkumar0703@gmail.com>
2026-02-28 02:27:28 +0530

    Update variables.tf

--- projectGCP/variables.tf
+++ projectGCP/variables.tf
@@ -2,3 +2,25 @@ variable "project_id" { type = string }
 variable "region" { type = string }
 variable "zone" { type = string }
 variable "env_suffix" { type = string }
+
+variable "vpcs" {
+  type = map(object({
+    name = string
+    subnets = map(object({
+      name = string
+      cidr = string
+    }))
+  }))
+}
+
+variable "firewall_rules" {
+  type = map(object({
+    network_name  = string
+    source_ranges = list(string)
+    target_tags   = list(string)
+    allows = list(object({
+      protocol = string
+      ports    = list(string)
+    }))
+  }))
+}
