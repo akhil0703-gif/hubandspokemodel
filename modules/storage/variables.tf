7639cc28801a94d33f1cdda1c18753c78b0c0302
akhil0703-gif <akhilkumar0703@gmail.com>
2026-02-28 02:22:33 +0530

    Create variables.tf

---
+++ modules/storage/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" { 
+  type        = string
+  description = "The name of the Google Cloud Storage bucket"
+}
+
+variable "location" { 
+  type        = string
+  description = "The location of the bucket"
+}
