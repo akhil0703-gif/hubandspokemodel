c9d1cdc00d5d80f243ce78be0d9195ca20227094
akhil0703-gif <akhilkumar0703@gmail.com>
2026-02-28 02:28:08 +0530

    Update outputs.tf

--- projectGCP/outputs.tf
+++ projectGCP/outputs.tf
@@ -1,7 +1,14 @@
 output "bastion_internal_ip" {
-  value = google_compute_instance.bastion_host.network_interface[0].network_ip
+  description = "Internal IP of the Bastion Host"
+  value       = google_compute_instance.bastion_host.network_interface[0].network_ip
 }
 
 output "spoke2_vm_internal_ip" {
-  value = google_compute_instance.spoke2_backend_vm.network_interface[0].network_ip
+  description = "Internal IP of the Spoke 2 Backend VM"
+  value       = google_compute_instance.spoke2_backend_vm.network_interface[0].network_ip
+}
+
+output "storage_bucket_url" {
+  description = "GCS Bucket URL"
+  value       = module.storage.bucket_url
 }
